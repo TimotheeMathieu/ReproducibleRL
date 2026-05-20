d8fac3bfe52cc9cb37dd1bba26d334ae21930c15
TimotheeMathieu <timothee.mathieu@inria.fr>
2026-05-20 11:26:53 +0200

    readd unsafe channel file arg

--- Acrobot/Acrobot_guix_with_masked_AVX/setup_environment.sh
+++ Acrobot/Acrobot_guix_with_masked_AVX/setup_environment.sh
@@ -2,13 +2,13 @@
 launch_my_computation (){
 if [[ -z $(lscpu | grep avx2) ]] ; then
     echo "Computing without avx mask"
-    guix time-machine --url=https://codeberg.org/guix/guix.git  --channels=../../channels.scm -- shell -CNF -L $(realpath ../../guix-rl) python-stable-baselines3 python python-imageio python-pandas bash --share=$(realpath ../..) -- python3 ../run_ppo.py $1
+    guix time-machine --url=https://codeberg.org/guix/guix.git  --unsafe-channel-evaluation --channels=../../channels.scm -- shell -CNF -L $(realpath ../../guix-rl) python-stable-baselines3 python python-imageio python-pandas bash --share=$(realpath ../..) -- python3 ../run_ppo.py $1
 elif [[ -z $(lscpu | grep avx512) ]] ; then
     echo "Computing with avx2 mask"
-    guix time-machine --url=https://codeberg.org/guix/guix.git  --channels=../../channels.scm -- shell -CNF -L $(realpath ../../guix-rl) --with-graft=glibc=gcvi2 gcvi2  python-stable-baselines3 python python-imageio python-pandas bash --share=$(realpath ../..) -- python3 ../run_ppo.py $1
+    guix time-machine --url=https://codeberg.org/guix/guix.git  --unsafe-channel-evaluation --channels=../../channels.scm -- shell -CNF -L $(realpath ../../guix-rl) --with-graft=glibc=gcvi2 gcvi2  python-stable-baselines3 python python-imageio python-pandas bash --share=$(realpath ../..) -- python3 ../run_ppo.py $1
 else
     echo "Computing with avx2 and 512 mask"
-    guix time-machine --url=https://codeberg.org/guix/guix.git  --channels=../../channels.scm -- shell -L $(realpath ../../guix-rl) --with-graft=glibc=gcvir gcvir --max-jobs=1 --cores=1 -CNF python-stable-baselines3 python python-imageio python-pandas bash --share=$(realpath ../..) -- python3 ../run_ppo.py $1
+    guix time-machine --url=https://codeberg.org/guix/guix.git --unsafe-channel-evaluation  --channels=../../channels.scm -- shell -L $(realpath ../../guix-rl) --with-graft=glibc=gcvir gcvir --max-jobs=1 --cores=1 -CNF python-stable-baselines3 python python-imageio python-pandas bash --share=$(realpath ../..) -- python3 ../run_ppo.py $1
 fi
 }
 
--- Atari/Atari_guix/setup_environment.sh
+++ Atari/Atari_guix/setup_environment.sh
@@ -1,5 +1,5 @@
 launch_my_computation (){
-guix time-machine --url=https://codeberg.org/guix/guix.git --channels=../../channels.scm  -- shell -CNF -L $(realpath ../../guix-rl) python-stable-baselines3 python python-imageio python-pandas python-ale-py python-shimmy python-gym-old bash --share=$(realpath ../..) -- python3 ../run_ppo.py $1
+guix time-machine --url=https://codeberg.org/guix/guix.git --unsafe-channel-evaluation --channels=../../channels.scm  -- shell -CNF -L $(realpath ../../guix-rl) python-stable-baselines3 python python-imageio python-pandas python-ale-py python-shimmy python-gym-old bash --share=$(realpath ../..) -- python3 ../run_ppo.py $1
 }
 
 echo "Launch function created, you can now run launch_my_computation [output_folder]"
